9108e4bab26ad15b2a4f1e80a1b90d3c3d7d1807
PGCayres <129511939+giotpos01@users.noreply.github.com>
2025-07-26 00:03:04 -0300

    Add files via upload

---
+++ M03_pipeline_de_treinamento/dataset.py
@@ -0,0 +1,62 @@
+import random
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+
+
+class Subset(Dataset):
+    """Cria um subconjunto de um dataset."""
+
+    def __init__(self, ds, indices, transform=None):
+        self.ds = ds
+        self.indices = indices
+        self.transform = transform
+
+    def __getitem__(self, idx):
+
+        img, target = self.ds[self.indices[idx]]
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.indices)
+    
+class Transform:
+    """Transforma as imagens de um dataset."""
+
+    def __init__(self, mean, std):
+
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        """Transformação PIL->numpy->tensor->z-score."""
+
+        # Conversão PIL->numpy
+        img = np.array(img, dtype=np.float32)
+        # Conversão numpy->pytorch
+        img = torch.from_numpy(img)
+        # Normalização
+        img = (img-self.mean)/self.std
+        # Adição de um canal
+        img = img.reshape(1, img.shape[0], img.shape[1])
+
+        return img
+
+def load_mnist(root="../data", n=1000):
+    """Carrega o dataset MNIST e cria subconjuntos de treino e validação."""
+
+    MEAN = 33.0
+    STD = 76.5
+
+    ds = datasets.MNIST(root, train=True, download=True)
+    random.seed(42)
+    indices = random.sample(range(len(ds)), k=2*n)
+    ds_train = Subset(ds, indices[:n], Transform(MEAN, STD))
+    ds_valid = Subset(ds, indices[n:], Transform(MEAN, STD))
+
+    return ds_train, ds_valid
